aa4d43ec337cb68c3dc3b6f552ec70c2efe78c82
AbdullahAymn <86365518+AbdullahAymn@users.noreply.github.com>
2023-08-20 01:16:47 +0300

    fix 1

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import Cookies from "js-cookie";
 import { useRouter } from "next/navigation";
 import LogIn from "./components/Login";
@@ -8,9 +8,12 @@ export default function Home() {
   const token = Cookies.get("token") || false;
   const router = useRouter();
 
-  if (token) {
-    router.push("/main");
-    return;
-  } 
-  return <LogIn />
+  useEffect(() => {
+    if (token) {
+      router.push("/main");
+      return;
+    }
+  }, []);
+
+  return <LogIn />;
 }
