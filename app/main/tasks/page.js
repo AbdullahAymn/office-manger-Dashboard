48bf484078d980e745df7cb8a3fc4a0bbc9ce85c
AbdullahAymn <86365518+AbdullahAymn@users.noreply.github.com>
2023-08-20 16:31:21 +0300

    task api

--- app/main/tasks/page.js
+++ app/main/tasks/page.js
@@ -1,21 +1,44 @@
 "use client";
 import NameAndSearch from "@/app/components/NameAndSearch";
 import React, { useContext, useState } from "react";
-import { tempTasksData } from "./tempData";
 import { isArabic } from "@/utils/langStore";
 import Popup from "reactjs-popup";
 import Delete from "@/app/components/popup/delete";
 import AddAndEdit from "@/app/components/popup/AddAndEdit";
+import Loader from "@/app/components/Loader";
+import Cookies from "js-cookie";
+import { useEffect } from "react";
 
 export default function Tasks() {
   const isArabicprop = useContext(isArabic).arabic;
+  const [loader, setLoader] = useState(false);
+  const [refresh, setRefresh] = useState(false);
   //
   //
   // get Data
   //
   //
 
-  const [tasksData, setTasksData] = useState(tempTasksData);
+  const [tasksData, setTasksData] = useState([]);
+  const [jobsDataforserch, setJobsDatasforserch] = useState([]);
+  const token = Cookies.get("token");
+  const myHeaders = new Headers();
+  myHeaders.append("Authorization", `Bearer ${token}\n`);
+  useEffect(() => {
+    setLoader(!loader);
+    fetch(`https://backend2.dasta.store/api/auth/basicInfoFetchtask`, {
+      method: "GET",
+      headers: myHeaders,
+    }).then((res) => {
+      if (res.status === 200) {
+        res.json().then((data) => {
+          setTasksData(data);
+          setJobsDatasforserch(data);
+        });
+        setLoader(false);
+      }
+    });
+  }, [refresh]);
   const [slice, setSlice] = useState([]);
 
   const getSlice = (slice) => {
@@ -27,7 +50,7 @@ export default function Tasks() {
   };
 
   const restSearch = () => {
-    setTasksData(tempTasksData);
+    setTasksData(jobsDataforserch);
   };
 
   //
@@ -42,34 +65,44 @@ export default function Tasks() {
   const toggelOpenAdd = () => {
     setOpenAdd(!openAdd);
   };
+  const toggelOpenAddresfresh = () => {
+    setOpenAdd(!openAdd);
+    setRefresh(!refresh);
+  };
 
   //delete
 
   const [openDelet, setOpenDelete] = useState(false);
-  const [deleteditemName, setdeletedItemName] = useState("");
+  const [deleteditem, setdeletedItem] = useState("");
 
   const openDeleteHandeller = (e) => {
-    setdeletedItemName(e.name);
+    setdeletedItem(e);
     setOpenDelete(!openDelet);
   };
 
   const closeDeleteHandeller = () => {
     setOpenDelete(!openDelet);
   };
+  const closrefresh = () => {
+    setOpenDelete(!openDelet);
+    setRefresh(!refresh);
+  };
 
   //edit
   const [openEdit, setOpenEdit] = useState(false);
-  const [editedItemName, setEditedItemName] = useState("");
-  const [editedItemNameEn, setEditedItemNameEn] = useState("");
+  const [editedItem, setEditedItem] = useState("");
 
   const openEditHandeller = (e) => {
     setOpenEdit(!openEdit);
-    setEditedItemName(e.nameAr);
-    setEditedItemNameEn(e.nameEn);
+    setEditedItem(e);
   };
 
   const closeEditHandeller = () => {
     setOpenEdit(!openEdit);
+  };
+  const closeEditRefresh = () => {
+    setOpenEdit(!openEdit);
+    setRefresh(!refresh);
   };
 
   //
@@ -79,7 +112,9 @@ export default function Tasks() {
   //
   const tabelData = slice.map((e) => (
     <tr key={e.id} className="grid grid-cols-9 p-2">
-      <td className=" col-span-7 md:col-span-8 text-start">{ isArabicprop ?e.nameAr : e.nameEn}</td>
+      <td className=" col-span-7 md:col-span-8 text-start">
+        {isArabicprop ? e.name : e.name_en}
+      </td>
       <td className=" col-span-2 md:col-span-1 text-center text-black/70">
         <i
           onClick={() => openDeleteHandeller(e)}
@@ -94,6 +129,9 @@ export default function Tasks() {
   ));
   return (
     <div>
+      <Popup open={loader}>
+        <Loader />
+      </Popup>
       {/*  */}
       {/*  */}
       {/* The top section */}
@@ -102,7 +140,11 @@ export default function Tasks() {
       <div>
         {/* Add PopUp */}
         <Popup open={openAdd}>
-          <AddAndEdit close={toggelOpenAdd} />
+          <AddAndEdit
+            link={"basicInfoAddtask"}
+            refresh={toggelOpenAddresfresh}
+            close={toggelOpenAdd}
+          />
         </Popup>
 
         {/* the Body */}
@@ -112,7 +154,7 @@ export default function Tasks() {
           reset={restSearch}
           addFun={toggelOpenAdd}
           data={tasksData}
-          dataForSearch={tempTasksData}
+          dataForSearch={jobsDataforserch}
           name={isArabicprop ? "المهام" : "Tasks"}
           subName={isArabicprop ? "المهام" : "Tasks"}
           ti={isArabicprop ? "المهمة" : "Task"}
@@ -131,17 +173,23 @@ export default function Tasks() {
         {/* delete */}
 
         <Popup open={openDelet}>
-          <Delete close={closeDeleteHandeller} branch={deleteditemName} />
+          <Delete
+            close={closeDeleteHandeller}
+            refresh={closrefresh}
+            link={"basicInfoDeletetask"}
+            element={deleteditem}
+          />
         </Popup>
 
         {/* edit */}
 
         <Popup open={openEdit}>
           <AddAndEdit
+            link="basicInfoUpdatetask"
             edit={true}
-            name={editedItemName}
-            nameEn={editedItemNameEn}
+            element={editedItem}
             close={closeEditHandeller}
+            refresh={closeEditRefresh}
           />
         </Popup>
 
