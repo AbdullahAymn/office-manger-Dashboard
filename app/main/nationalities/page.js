aed32225a7b0ec149c219ab25b70c5598cd1d1f4
AbdullahAymn <86365518+AbdullahAymn@users.noreply.github.com>
2023-08-20 16:03:50 +0300

    nationalites api

--- app/main/nationalities/page.js
+++ app/main/nationalities/page.js
@@ -3,19 +3,41 @@ import NameAndSearch from "@/app/components/NameAndSearch";
 import AddAndEdit from "@/app/components/popup/AddAndEdit";
 import Delete from "@/app/components/popup/delete";
 import { isArabic } from "@/utils/langStore";
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Popup from "reactjs-popup";
-import { tempNatData } from "./tempdata";
+import Loader from "@/app/components/Loader";
+import Cookies from "js-cookie";
 
 export default function page() {
   const isArabicprop = useContext(isArabic).arabic;
+  const [loader, setLoader] = useState(false);
+  const [refresh, setRefresh] = useState(false);
   //
   //
   // get Data
   //
   //
 
-  const [natinaltyData, setnatinaltyData] = useState(tempNatData);
+  const [natinaltyData, setnatinaltyData] = useState([]);
+  const [jobsDataforserch, setJobsDatasforserch] = useState([]);
+  const token = Cookies.get("token");
+  const myHeaders = new Headers();
+  myHeaders.append("Authorization", `Bearer ${token}\n`);
+  useEffect(() => {
+    setLoader(!loader);
+    fetch(`https://backend2.dasta.store/api/auth/basicInfoFetchnationalite`, {
+      method: "GET",
+      headers: myHeaders,
+    }).then((res) => {
+      if (res.status === 200) {
+        res.json().then((data) => {
+          setnatinaltyData(data);
+          setJobsDatasforserch(data);
+        });
+        setLoader(false);
+      }
+    });
+  }, [refresh]);
   const [slice, setSlice] = useState([]);
 
   const getSlice = (slice) => {
@@ -27,7 +49,7 @@ export default function page() {
   };
 
   const restSearch = () => {
-    setnatinaltyData(tempNatData);
+    setnatinaltyData(jobsDataforserch);
   };
 
   //
@@ -41,34 +63,44 @@ export default function page() {
   const toggelOpenAdd = () => {
     setOpenAdd(!openAdd);
   };
+  const toggelOpenAddresfresh = () => {
+    setOpenAdd(!openAdd);
+    setRefresh(!refresh);
+  };
 
   //delete
 
   const [openDelet, setOpenDelete] = useState(false);
-  const [deleteditemName, setdeletedItemName] = useState("");
+  const [deleteditem, setdeletedItem] = useState("");
 
   const openDeleteHandeller = (e) => {
-    setdeletedItemName(e.name);
+    setdeletedItem(e);
     setOpenDelete(!openDelet);
   };
 
   const closeDeleteHandeller = () => {
     setOpenDelete(!openDelet);
   };
+  const closrefresh = () => {
+    setOpenDelete(!openDelet);
+    setRefresh(!refresh);
+  };
 
   //edit
   const [openEdit, setOpenEdit] = useState(false);
-  const [editedItemName, setEditedItemName] = useState("");
-  const [editedItemNameEn, setEditedItemNameEn] = useState("");
+  const [editedItem, setEditedItem] = useState("");
 
   const openEditHandeller = (e) => {
     setOpenEdit(!openEdit);
-    setEditedItemName(e.nameAr);
-    setEditedItemNameEn(e.nameEn);
+    setEditedItem(e);
   };
 
   const closeEditHandeller = () => {
     setOpenEdit(!openEdit);
+  };
+  const closeEditRefresh = () => {
+    setOpenEdit(!openEdit);
+    setRefresh(!refresh);
   };
 
   //
@@ -78,7 +110,9 @@ export default function page() {
   //
   const tabelData = slice.map((e) => (
     <tr key={e.id} className="grid grid-cols-9 p-2">
-      <td className=" col-span-7 md:col-span-8 text-start">{isArabicprop?e.nameAr : e.nameEn}</td>
+      <td className=" col-span-7 md:col-span-8 text-start">
+        {isArabicprop ? e.name : e.name_en}
+      </td>
       <td className=" col-span-2 md:col-span-1 text-center text-black/70">
         <i
           onClick={() => openDeleteHandeller(e)}
@@ -94,6 +128,9 @@ export default function page() {
 
   return (
     <div>
+      <Popup open={loader}>
+        <Loader />
+      </Popup>
       {/*  */}
       {/*  */}
       {/* The top section */}
@@ -102,7 +139,11 @@ export default function page() {
       <div>
         {/* Add PopUp */}
         <Popup open={openAdd}>
-          <AddAndEdit close={toggelOpenAdd} />
+          <AddAndEdit
+            link={"basicInfoAddnationalite"}
+            refresh={toggelOpenAddresfresh}
+            close={toggelOpenAdd}
+          />
         </Popup>
 
         {/* the Body */}
@@ -112,7 +153,7 @@ export default function page() {
           reset={restSearch}
           addFun={toggelOpenAdd}
           data={natinaltyData}
-          dataForSearch={tempNatData}
+          dataForSearch={jobsDataforserch}
           name={isArabicprop ? "الجنسيات" : "Nationalities"}
           subName={isArabicprop ? "الجنسيات" : "Nationalities"}
           ti={isArabicprop ? "الجنسية" : "Nationality"}
@@ -131,7 +172,12 @@ export default function page() {
 
         {/* delete */}
         <Popup open={openDelet}>
-          <Delete close={closeDeleteHandeller} branch={deleteditemName} />
+          <Delete
+            refresh={closrefresh}
+            close={closeDeleteHandeller}
+            link={"basicInfoDeletenationalite"}
+            element={deleteditem}
+          />
         </Popup>
 
         {/* edit */}
@@ -139,9 +185,10 @@ export default function page() {
         <Popup open={openEdit}>
           <AddAndEdit
             edit={true}
-            name={editedItemName}
-            nameEn={editedItemNameEn}
+            element={editedItem}
             close={closeEditHandeller}
+            refresh={closeEditRefresh}
+            link="basicInfoUpdatenationalite"
           />
         </Popup>
 
