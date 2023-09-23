dad5962750d28c2252a66804d17ee3c2784eb92f
AbdullahAymn <86365518+AbdullahAymn@users.noreply.github.com>
2023-09-23 18:01:23 +0300

    fix total delay

--- app/main/totaldelay/page.js
+++ app/main/totaldelay/page.js
@@ -257,6 +257,118 @@ export default function DelayEarlyLeaveReport() {
         <Loader />
       </Popup>
       <ToastContainer position="bottom-center" theme="colored" />
+      <div>
+        <Label
+          headers={headers}
+          data={cvsData}
+          fileName="تقارير اجمالي التأخير "
+          pdf={printDocument}
+          setsearch={showSearchHandeller}
+          label={isArabicprop ? " اجمالي التأخير" : "Total Delay"}
+        />
+      </div>
+      {showSearch && (
+        <div className=" mb-12">
+          <div className=" relative w-full p-2 border rounded-md">
+            <i
+              onClick={resetHandeller}
+              className={`fa-solid fa-rotate-right hover:cursor-pointer absolute top-2 ${
+                isArabicprop ? " left-2" : " right-2"
+              }`}
+            ></i>
+            <form onSubmit={searchHandeller}>
+              <div className=" w-full p-2 pb-10 grid grid-cols-3 md:grid-cols-12">
+                <div className=" col-span-3 md:mx-4">
+                  <h4>{isArabicprop ? "الكود" : "Code"}</h4>
+                  <input
+                    className=" w-full p-2 border outline-none"
+                    value={code}
+                    onChange={(e) => setCode(e.target.value)}
+                    type="number"
+                    placeholder={isArabicprop ? "الكود" : "Code"}
+                  />
+                </div>
+                <div className=" col-span-3 md:mx-4">
+                  <h4>{isArabicprop ? "اسم الموظف:" : "Name :"}</h4>
+                  <input
+                    className=" w-full p-2 border outline-none"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    type="text"
+                    placeholder={isArabicprop ? "اسم الموظف" : "Name"}
+                  />
+                </div>
+                <div className=" col-span-3 md:mx-4">
+                  <h4>{isArabicprop ? "الفرع" : "Branch"} </h4>
+                  <select
+                    value={branch}
+                    required
+                    onChange={(e) => setBranch(e.target.value)}
+                    className=" p-2 rounded-md border outline-none w-full"
+                  >
+                    <option selected hidden>
+                      Choose one
+                    </option>
+                    {branchesOptions}
+                  </select>
+                </div>
+                <div className=" col-span-3 md:mx-4">
+                  <h4>{isArabicprop ? "الدوام" : " Working Time"}</h4>
+                  <select
+                    value={shift}
+                    required
+                    onChange={(e) => setShift(e.target.value)}
+                    className=" rounded-md p-2 border outline-none w-full"
+                  >
+                    <option selected hidden>
+                      Choose one
+                    </option>
+                    {workingTimeOptions}
+                  </select>
+                </div>
+                <div className=" col-span-3 md:mx-4">
+                  <h4>{isArabicprop ? "الوظيفة" : " Job "}</h4>
+                  <select
+                    value={job}
+                    onChange={(e) => setJob(e.target.value)}
+                    className=" rounded-md p-2 border outline-none w-full"
+                  >
+                    <option selected hidden>
+                      Choose one
+                    </option>
+                    {jobOptions}
+                  </select>
+                </div>
+                <div className=" col-span-3 md:mx-4">
+                  <h4>{isArabicprop ? "من :" : "From :"}</h4>
+                  <input
+                    value={from}
+                    onChange={(e) => setFrom(e.target.value)}
+                    type="date"
+                    className=" w-full p-2 border outline-none"
+                  />
+                </div>
+                <div className=" col-span-3 md:mx-4">
+                  <h4>{isArabicprop ? "إلى :" : "To :"}</h4>
+                  <input
+                    value={to}
+                    onChange={(e) => setTo(e.target.value)}
+                    type="date"
+                    className=" w-full p-2 border outline-none"
+                  />
+                </div>
+              </div>
+              <button
+                className={`hover:cursor-pointer absolute bottom-2 bg-sky-400 text-white px-12 py-1 rounded-full ${
+                  isArabicprop ? " left-2" : " right-2"
+                }`}
+              >
+                {isArabicprop ? "بحث" : "Search"}
+              </button>
+            </form>
+          </div>
+        </div>
+      )}
 
       <div>{dataShow}</div>
     </div>
